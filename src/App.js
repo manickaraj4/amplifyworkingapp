eff0552185b6e715923ed510bf8f1c59ceefb208
Manickaraj K M <manickgm@amazon.com>
2022-05-27 11:10:39 +0530

    added auth

--- src/App.js
+++ src/App.js
@@ -1,5 +1,30 @@
 import logo from './logo.svg';
 import './App.css';
+import "@aws-amplify/ui-react/styles.css";
+import {
+  withAuthenticator,
+  Button,
+  Heading,
+  Image,
+  View,
+  Card,
+} from "@aws-amplify/ui-react";
+
+function App({ signOut }) {
+  return (
+    <View className="App">
+      <Card>
+        <Image src={logo} className="App-logo" alt="logo" />
+        <Heading level={1}>We now have Auth!</Heading>
+      </Card>
+      <Button onClick={signOut}>Sign Out</Button>
+    </View>
+  );
+}
+
+export default withAuthenticator(App);
+
+/*
 
 function App() {
   return (
@@ -23,3 +48,5 @@ function App() {
 }
 
 export default App;
+
+*/
