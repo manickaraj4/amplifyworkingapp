0753d2eeb8f36ae52ed1e0a16698b65a82ffe00d
Manickaraj K M <manickgm@amazon.com>
2022-08-23 21:24:20 +0530

    new

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 
 import logo from './logo.svg';
-import "@aws-amplify/ui-react/styles.css";
+//import "@aws-amplify/ui-react/styles.css";
 import {
   ThemeProvider, Text, Flex, Badge, StepperField, useTheme,
   withAuthenticator,
